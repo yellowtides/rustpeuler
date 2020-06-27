b8a2f2f837cdd92716d6868f0cf6c31d69b7a647
yellowtides <andi.dicu12@gmail.com>
2020-06-27 17:32:09 +0300

    first 15% problem done! :DD

--- src/sol/s0051.rs
+++ src/sol/s0051.rs
@@ -8,7 +8,11 @@
         > if no, continue
     in total, there should be < 20 million operations performed (~1s) */
 
-const ALPHABET: [char; 11] = ['*', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+use crate::base::compute_primeset;
+use std::collections::HashSet;
+use std::cmp::min;
+
+const ALPHABET: [char; 11] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '*'];
 const MAX_SIZE: usize = 6;
 
 fn fetch_words() -> Vec<String> {
@@ -41,22 +45,55 @@ fn fetch_words() -> Vec<String> {
     ans
 }
 
-// fn check_family(num: String) -> bool {
+fn check_family(num: String, primeset: &HashSet<u64>) -> (u64, u64) {
     
-//     let st: usize = 0;
+    let mut st: usize = 0;
 
-//     if num[0] == '*' {
-//         st = 1;
-//     }
-//     // edge case: *53 with * = 0 is not a prime, it is an invalid number
+    let num = num.trim_start_matches('0');
+    // trim all beginning 0s
 
-//     for wildcard in st..10 {
-//         if replace_p
-//     }
-// }
+    if num.chars().next().unwrap() == '*' {
+        st = 1; 
+    }
+    // edge case: *53 with * = 0 is not a prime, it is an invalid number
+
+    let mut n_primes: u64 = 0;
+    let mut smallest_prime: u64 = 0 as u64;
+    
+    for wildcard in st..10 {
+        
+        let aux = &num;
+        let aux = &aux.replace("*", &((wildcard as u8 + '0' as u8) as char).to_string());
+        
+        
+        let candidate = aux.parse::<u64>().unwrap();
+        if primeset.contains(&candidate) {
+
+            if smallest_prime == 0 {
+                smallest_prime = candidate;
+            } 
+            n_primes += 1;
+        }
+        // counts the family primes by comparing them against the hashset
+    }
+
+    (n_primes, smallest_prime)
+}
 
 pub fn solve() -> u64 {
     
-    println!("{:?}", fetch_words());
-    0
+    let dictionary: Vec<String> = fetch_words();
+    let primeset: HashSet<u64> = compute_primeset(1e6 as usize);
+    let mut ans: u64 = 1e9 as u64;
+
+    for x in dictionary {
+
+        let packet = check_family(x, &primeset);
+
+        if packet.0 == 8 {
+            ans = min(ans, packet.1);
+        }
+    }
+
+    ans
 }
